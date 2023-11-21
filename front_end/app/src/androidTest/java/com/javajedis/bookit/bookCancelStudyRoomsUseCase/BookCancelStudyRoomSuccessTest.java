3dd9e5921de10b6af1c485123ad7b8a8f68d6df6
Tongyu Li <tonyli990322@gmail.com>
2023-11-20 21:01:39 -0800

    use UIAutomator to click outside of our app

--- front_end/app/src/androidTest/java/com/javajedis/bookit/bookCancelStudyRoomsUseCase/BookCancelStudyRoomSuccessTest.java
+++ front_end/app/src/androidTest/java/com/javajedis/bookit/bookCancelStudyRoomsUseCase/BookCancelStudyRoomSuccessTest.java
@@ -5,11 +5,8 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -17,12 +14,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
-import android.support.test.InstrumentationRegistry;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
-
-import static org.hamcrest.Matchers.not;
 
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.NoMatchingViewException;
@@ -31,10 +25,9 @@ import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
-import androidx.test.uiautomator.UiObject;
-import androidx.test.uiautomator.UiObjectNotFoundException;
-import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.UiObject2;
 
 import com.javajedis.bookit.MainActivity;
 import com.javajedis.bookit.R;
@@ -45,7 +38,6 @@ import com.javajedis.bookit.util.ToastMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -55,6 +47,10 @@ import java.time.format.DateTimeFormatter;
 
 import java.time.LocalDate;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import com.javajedis.bookit.model.BookingsModel;
+import com.javajedis.bookit.recyclerview.adapter.BookingsRecyclerViewAdapter;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -66,29 +62,30 @@ public class BookCancelStudyRoomSuccessTest {
 
     @Test
     public void bookCancelStudyRoomSuccessTest() {
+        ViewInteraction ic = onView(
+                allOf(withText("Sign in"),
+                        childAtPosition(
+                                allOf(withId(R.id.sign_in_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        ic.perform(click());
+
+        // TODO: need to be tested
         // ChatGPT usage: Yes --> from here
-        try {
-            // Initialize UiDevice instance
-            UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        // Initialize UiDevice instance
+        UiDevice uiDevice = UiDevice.getInstance(androidx.test.platform.app.InstrumentationRegistry.getInstrumentation());
+        // Wait for the Google Sign-In screen to appear
+        uiDevice.waitForIdle();
 
-            // Find and click on the option you want (for example, the "Sign in with Google" button)
-            UiObject googleSignInButton = uiDevice.findObject(new UiSelector().text("Sign in with Google"));
-            googleSignInButton.click();
+        // Click on the first Google account in the account picker
+        UiObject2 googleAccount = uiDevice.findObject(By.textContains("gmail")); // Modify the selector as needed
 
-            // Wait for the Google Sign-In screen to appear
-            uiDevice.waitForIdle();
-
-            // Identify the UI element for the remembered email (assuming it's in an AutoCompleteTextView)
-            UiObject emailField = uiDevice.findObject(new UiSelector().className("android.widget.AutoCompleteTextView"));
-
-            // Click on the email field to bring up the list of remembered emails
-            emailField.click();
-
-            // Assuming the remembered email is the first item in the list, click on it
-            UiObject rememberedEmail = uiDevice.findObject(new UiSelector().index(0));
-            rememberedEmail.click();
-        } catch (UiObjectNotFoundException e) {
-            e.printStackTrace();
+        // Click on the Google account
+        if (googleAccount != null) {
+            googleAccount.click();
         }
 
         // open search activity
@@ -149,10 +146,9 @@ public class BookCancelStudyRoomSuccessTest {
                         withParent(withParent(withId(android.R.id.content))),
                         isDisplayed()));
         recyclerView4.check(matches(isDisplayed()));
-        /**
-        // check book now is displayed in position 47;
-        onView(withId(R.id.timeslots_recycler_view)).check(new RecyclerViewItemCountAssertion(47));
-        */
+
+        onView(withId(R.id.timeslots_recycler_view)).check(new recyclerViewItemCountAssertion(47));
+
         // click the last time slots 23:30 - 24:00
         ViewInteraction recyclerView5 = onView(
                 allOf(withId(R.id.timeslots_recycler_view),
@@ -160,52 +156,76 @@ public class BookCancelStudyRoomSuccessTest {
                                 withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
                                 0)));
         recyclerView5.perform(actionOnItemAtPosition(47, click()));
-        // check booking screen opens successfully
-        ViewInteraction recyclerView6 = onView(
-                allOf(withId(R.id.bookings_recyclerView),
-                        withParent(withParent(withId(android.R.id.content))),
-                        isDisplayed()));
-        recyclerView6.check(matches(isDisplayed()));
 
-        ViewInteraction viewGroup = onView(
-                allOf(withParent(allOf(withId(R.id.bookings_recyclerView),
-                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
-                        isDisplayed()));
-        viewGroup.check(matches(isDisplayed()));
+//        ViewInteraction textView2 = onView(
+//                allOf(withId(R.id.action_bookings_textView), withText("click to cancel"),
+//                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
+//                        isDisplayed()));
+//        textView2.check(matches(withText("click to cancel")));
+//
+//        ViewInteraction textView3 = onView(
+//                allOf(withId(R.id.timeslot_bookings_textView), withText("2330-2400"),
+//                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
+//                        isDisplayed()));
+//        textView3.check(matches(withText("2330-2400")));
+//
+//        ViewInteraction textView4 = onView(
+//                allOf(withId(R.id.date_bookings_textView), withText("30-11-2023"),
+//                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
+//                        isDisplayed()));
+//        textView4.check(matches(withText("30-11-2023")));
 
-        ViewInteraction textView2 = onView(
-                allOf(withId(R.id.action_bookings_textView), withText("click to cancel"),
-                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
-                        isDisplayed()));
-        textView2.check(matches(withText("click to cancel")));
 
-        ViewInteraction textView3 = onView(
-                allOf(withId(R.id.timeslot_bookings_textView), withText("2330-2400"),
-                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
-                        isDisplayed()));
-        textView3.check(matches(withText("2330-2400")));
-
-        ViewInteraction textView4 = onView(
-                allOf(withId(R.id.date_bookings_textView), withText("30-11-2023"),
-                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
-                        isDisplayed()));
-        textView4.check(matches(withText("30-11-2023")));
-
+        // assert if current time is before 2330-2400
+        // ChatGPT usage: Yes --> from here
         // get current date
         LocalDate currentDate = LocalDate.now();
         LocalDate targetDate = LocalDate.of(2023, 11, 30);
 
-        // Assert that the current date is before 30-11-2023
-        assertTrue(currentDate.isBefore(targetDate));
+        if (currentDate.isBefore(targetDate)) {
+            // Assert that the current date is before 30-11-2023
+            assertTrue(currentDate.isBefore(targetDate));
+        } else if (currentDate.isEqual(targetDate)){
+            // Same dateAssert that the current time is before 23:30
+            String currentTime = getCurrentTime();
+            int currentHour = Integer.parseInt(currentTime.substring(0, 2));
+            int currentMinute = Integer.parseInt(currentTime.substring(2));
+
+            int targetHour = 23;
+            int targetMinute = 30;
+
+            // Convert both current time and target time to minutes for easy comparison
+            int currentTimeInMinutes = currentHour * 60 + currentMinute;
+            int targetTimeInMinutes = targetHour * 60 + targetMinute;
+
+            assertTrue(currentTimeInMinutes < targetTimeInMinutes);
+        } else {
+            // Test fails
+            fail("current time is after booking starting time for newly booked session");
+        }
 
         // ChatGPT usage: Yes --> from here
-        // click cancel booking
-        ViewInteraction recyclerView7 = onView(
-                allOf(withId(R.id.bookings_recyclerView),
-                        childAtPosition(
-                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                0)));
-        recyclerView7.perform(actionOnItemAtPosition(0, click()));
+
+        onView(withId(R.id.bookings_recyclerView)).check(new bookingsRecyclerViewItemCountAssertion(0));
+
+        // TODO: need to be tested
+        uiDevice.waitForIdle();
+
+        // Click on the first bookings in the bookings page
+        UiObject2 firstBooking = uiDevice.findObject(By.textContains("click to cancel")); // Modify the selector as needed
+
+        // Click on the firstBooking
+        if (firstBooking != null) {
+            firstBooking.click();
+        }
+
+//        // click cancel booking
+//        ViewInteraction recyclerView7 = onView(
+//                allOf(withId(R.id.bookings_recyclerView),
+//                        childAtPosition(
+//                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+//                                0)));
+//        recyclerView7.perform(actionOnItemAtPosition(0, click()));
 
         // From: https://www.qaautomated.com/2016/01/how-to-test-toast-message-using-espresso.html
         // check if the message appears with the text: “Your booking has been canceled!”
@@ -213,7 +233,7 @@ public class BookCancelStudyRoomSuccessTest {
                 .check(matches(isDisplayed()));
     }
 
-    // From ChatGPT
+    // ChatGPT usage: Yes
     public static class recyclerViewItemCountAssertion implements ViewAssertion {
         private final int position;
 
@@ -234,6 +254,33 @@ public class BookCancelStudyRoomSuccessTest {
                 // Now you can perform assertions or actions based on the data in the item
                 // For example, you can check a specific property of the item
                 assertThat(item.getStatus(), is("book now"));
+            } else {
+                throw noViewFoundException;
+            }
+        }
+    }
+
+    // ChatGPT usage: Yes
+    public static class bookingsRecyclerViewItemCountAssertion implements ViewAssertion {
+        private final int position;
+
+        public bookingsRecyclerViewItemCountAssertion(int position) {
+            this.position = position;
+        }
+
+        @Override
+        public void check(View view, NoMatchingViewException noViewFoundException) {
+            if (view instanceof RecyclerView) {
+                RecyclerView recyclerView = (RecyclerView) view;
+                RecyclerView.Adapter adapter = recyclerView.getAdapter();
+
+                // Assuming your adapter is YourAdapter
+                assert adapter != null;
+                BookingsModel item = ((BookingsRecyclerViewAdapter) adapter).getItemAtPosition(position);
+
+                // Now you can perform assertions or actions based on the data in the item
+                // For example, you can check a specific property of the item
+                assertThat(item.getAction(), is("click to cancel"));
             } else {
                 throw noViewFoundException;
             }
