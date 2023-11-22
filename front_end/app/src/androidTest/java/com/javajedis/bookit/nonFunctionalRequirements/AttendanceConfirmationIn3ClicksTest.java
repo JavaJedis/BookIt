b2923eaf3d09014a5b4731eb88670cc277fc4b7a
Tongyu Li <tonyli990322@gmail.com>
2023-11-21 23:01:02 -0800

    update nonfunctional testing files

--- front_end/app/src/androidTest/java/com/javajedis/bookit/nonFunctionalRequirements/AttendanceConfirmationIn3ClicksTest.java
+++ front_end/app/src/androidTest/java/com/javajedis/bookit/nonFunctionalRequirements/AttendanceConfirmationIn3ClicksTest.java
@@ -1,3 +1,148 @@
 package com.javajedis.bookit.nonFunctionalRequirements;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject2;
+
+import com.javajedis.bookit.MainActivity;
+import com.javajedis.bookit.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
 public class AttendanceConfirmationIn3ClicksTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Rule
+    public GrantPermissionRule mGrantPermissionRule =
+            GrantPermissionRule.grant(
+                    "android.permission.ACCESS_FINE_LOCATION",
+                    "android.permission.ACCESS_COARSE_LOCATION");
+
+    @Test
+    public void attendanceConfirmationIn3ClicksTest() {
+        int clickCount = 0;
+        ViewInteraction ic = onView(
+                allOf(withText("Sign in"),
+                        childAtPosition(
+                                Matchers.allOf(ViewMatchers.withId(R.id.sign_in_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        ic.perform(click());
+
+        // ChatGPT usage: Yes --> from here
+        // Initialize UiDevice instance
+        UiDevice uiDevice = UiDevice.getInstance(androidx.test.platform.app.InstrumentationRegistry.getInstrumentation());
+        // Wait for the Google Sign-In screen to appear
+        uiDevice.waitForIdle();
+
+        // Click on the first Google account in the account picker
+        UiObject2 googleAccount = uiDevice.findObject(By.textContains("@")); // Modify the selector as needed
+
+        // Click on the Google account
+        if (googleAccount != null) {
+            googleAccount.click();
+        }
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.bookings_button), withText("bookings"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+        // first click: on “bookings” button in the MainActivity
+        clickCount++;
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.bookings_recyclerView),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        recyclerView.check(matches(isDisplayed()));
+
+        ViewInteraction viewGroup = onView(
+                allOf(withParent(allOf(withId(R.id.bookings_recyclerView),
+                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
+                        isDisplayed()));
+        viewGroup.check(matches(isDisplayed()));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.action_bookings_textView), withText("confirm attendance"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
+                        isDisplayed()));
+        textView2.check(matches(withText("confirm attendance")));
+
+        //  Click on the confirm attendance button
+        UiObject2 confirmAttendance = uiDevice.findObject(By.textContains("confirm attendance")); // Modify the selector as needed
+
+        if (confirmAttendance != null) {
+            confirmAttendance.click();
+        }
+        // second click: on the booking which confirms it
+        clickCount++;
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.action_bookings_textView), withText("confirmed"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
+                        isDisplayed()));
+        textView4.check(matches(withText("confirmed")));
+
+        assertTrue(clickCount <= 3);
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
 }
